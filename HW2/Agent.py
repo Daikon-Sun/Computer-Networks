f07f6742b148a2ddc29502661f4f767965640824
Daikon <sunfankeng@gmail.com>
2018-12-22 00:01:05 +0800

    finish report

--- HW2/Agent.py
+++ HW2/Agent.py
@@ -19,25 +19,25 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as agentSocket:
             agentSocket.sendto(packet, (args.receiver_ip, args.receiver_port))
 
 
-    packet_cnt, loss_cnt = 0, 0
+    packetCnt, lossCnt = 0, 0
 
     while True:
         length, seqNum, ackNum, fin, syn, ack, rawPacket, address, packet = \
             receive_and_unpack(agentSocket, args, sendOrg=True)
         if address[0] == args.sender_ip and address[1] == args.sender_port:
-            packet_cnt += 1
+            packetCnt += 1
             if fin == 1:
                 print('get\tfin')
                 send_packet(0)
                 print('fwd\tfin')
             else:
                 if random.random() < args.loss_rate:
-                    loss_cnt += 1
-                    print('drop\tdata\t#{},\tloss rate = {:4f}'.format(seqNum, loss_cnt / packet_cnt))
+                    lossCnt += 1
+                    print('drop\tdata\t#{},\tloss rate = {:4f}'.format(seqNum, lossCnt / packetCnt))
                 else:
                     print('get\tdata\t#{}'.format(seqNum))
                     send_packet(0)
-                    print('fwd\tdata\t#{},\tloss rate = {:4f}'.format(seqNum, loss_cnt / packet_cnt))
+                    print('fwd\tdata\t#{},\tloss rate = {:4f}'.format(seqNum, lossCnt / packetCnt))
 
         elif address[0] == args.receiver_ip and address[1] == args.receiver_port:
             if fin == 1:
--- HW2/utils.py
+++ HW2/utils.py
@@ -3,6 +3,27 @@ import hashlib
 import pickle
 
 
+def parse_args():
+    parser = argparse.ArgumentParser(description='TCP by UDP')
+    parser.add_argument('-ai', '--agent_ip', type=str, default='127.0.0.1')
+    parser.add_argument('-ap', '--agent_port', type=int, default=8888)
+    parser.add_argument('-bs', '--buffer_size', type=int, default=32)
+    parser.add_argument('-if', '--input_file', type=str, default='data/numbers.txt')
+    parser.add_argument('-lr', '--loss_rate', type=int, default=0.05)
+    parser.add_argument('-of', '--output_file', type=str, default='./data/result')
+    parser.add_argument('-ps', '--packet_size', type=int, default=1000)
+    parser.add_argument('-ri', '--receiver_ip', type=str, default='127.0.0.1')
+    parser.add_argument('-rp', '--receiver_port', type=int, default=8889)
+    parser.add_argument('-si', '--sender_ip', type=str, default='127.0.0.1')
+    parser.add_argument('-sp', '--sender_port', type=int, default=8887)
+    parser.add_argument('-th', '--threshold', type=int, default=16)
+    parser.add_argument('-to', '--time_out', type=int, default=1)
+
+    args = parser.parse_args()
+    args.output_file += args.input_file[args.input_file.rfind('.'):]
+    return args
+
+
 def pack(length, seqNum, ackNum, fin, syn, ack, rawPacket):
     return length.to_bytes(4, 'little', signed=True) \
          + seqNum.to_bytes(4, 'little', signed=True) \
@@ -30,27 +51,6 @@ def receive_and_unpack(socket, args, sendOrg=False):
         return length, seqNum, ackNum, fin, syn, ack, rawPacket, address, packet
     else:
         return length, seqNum, ackNum, fin, syn, ack, rawPacket, address
-
-
-def parse_args():
-    parser = argparse.ArgumentParser(description='TCP by UDP')
-    parser.add_argument('-ai', '--agent_ip', type=str, default='127.0.0.1')
-    parser.add_argument('-ap', '--agent_port', type=int, default=8888)
-    parser.add_argument('-bs', '--buffer_size', type=int, default=32)
-    parser.add_argument('-if', '--input_file', type=str, default='data/numbers.txt')
-    parser.add_argument('-lr', '--loss_rate', type=int, default=0.05)
-    parser.add_argument('-of', '--output_file', type=str, default='./data/result')
-    parser.add_argument('-ps', '--packet_size', type=int, default=1000)
-    parser.add_argument('-ri', '--receiver_ip', type=str, default='127.0.0.1')
-    parser.add_argument('-rp', '--receiver_port', type=int, default=8889)
-    parser.add_argument('-si', '--sender_ip', type=str, default='127.0.0.1')
-    parser.add_argument('-sp', '--sender_port', type=int, default=8887)
-    parser.add_argument('-th', '--threshold', type=int, default=16)
-    parser.add_argument('-to', '--time_out', type=int, default=1)
-
-    args = parser.parse_args()
-    args.output_file += args.input_file[args.input_file.rfind('.'):]
-    return args
 
 
 def send_to_agent(socket, packet, args):
